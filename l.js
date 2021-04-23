32526ff553b0992ccd5f807972fc2cfb6f03f14f
qwq0 <82009862+qwq0@users.noreply.github.com>
2021-04-23 16:00:34 +0800

    Update l.js

--- l.js
+++ l.js
@@ -9,7 +9,7 @@
 		cw["p_QwQ"] = "loaded";
 		var scr = cw.document.createElement("script");
 		scr.setAttribute("type", "text/javascript");
-		scr.setAttribute("src", "https://qwq0.rthe.xyz/iirose/main.js?" + Math.random());
+		scr.setAttribute("src", "https://qwq0.github.io/iiroseForge/core.js?" + Math.random());
 		cw.document.body.appendChild(scr);
 	};
 	setTimeout(f, 1000);
