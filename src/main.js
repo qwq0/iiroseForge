c89a50ea55829e791ff96e19efdf3d19d304f6bf
qwq0 <82009862+qwq0@users.noreply.github.com>
2023-04-09 18:57:58 +0800

    ud

--- src/main.js
+++ src/main.js
@@ -2,18 +2,31 @@ import { initInjectIframe } from "./injectIframe/initInjectIframe.js";
 import "./forgeApi/forgeApi.js";
 
 
-
-if (!window["iiroseForgeInjected"])
+if (location.host == "iirose.com")
 {
-    (/** @type {HTMLIFrameElement} */(document.getElementById("mainFrame"))).addEventListener("load", () => // 主iframe加载事件
+    if (localStorage.pathname == "/")
     {
-        console.log("[iiroseForge] 正在将iiroseForge注入iframe");
-        initInjectIframe();
-    });
+        if (!window["iiroseForgeInjected"])
+        {
+            (/** @type {HTMLIFrameElement} */(document.getElementById("mainFrame"))).addEventListener("load", () => // 主iframe加载事件
+            {
+                console.log("[iiroseForge] 正在将iiroseForge注入iframe");
+                initInjectIframe();
+            });
 
 
-    console.log("[iiroseForge] iiroseForge已启用");
-    window["iiroseForgeInjected"] = true; // 最外层页面已被注入标记
+            console.log("[iiroseForge] iiroseForge已启用");
+            window["iiroseForgeInjected"] = true; // 最外层页面已被注入标记
+        }
+        else
+            console.log("[iiroseForge] 已阻止重复注入");
+    }
+    else
+    {
+        console.log("[iiroseForge] 已阻止注入 iiroseForge需要注入至根页面的主上下文中");
+    }
 }
 else
-    console.log("[iiroseForge] 已阻止重复注入");
+{
+    console.log("[iiroseForge] 已阻止注入 iiroseForge仅支持蔷薇花园(iirose.com)");
+}
