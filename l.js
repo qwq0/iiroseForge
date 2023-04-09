948a77f33a0d128e4dd00a7c6d45ca4fb27f0fb4
qwq0 <82009862+qwq0@users.noreply.github.com>
2023-04-09 19:40:13 +0800

    ud

---
+++ l.js
@@ -0,0 +1,7 @@
+(function ()
+{
+	let script = document.createElement("script");
+	script.type = "text/javascript";
+	script.src = "https://qwq0.github.io/iiroseForge/iiroseForge.js?" + Math.random();
+	document.body.appendChild(script);
+})();
