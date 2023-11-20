6e00ab0a7cacd766e5d8af324bcf2f5781359e2d
qwq0 <82009862+qwq0@users.noreply.github.com>
2023-11-20 16:25:29 +0800

    Create inject-iiroseForge.user.js

---
+++ inject-iiroseForge.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         inject iiroseForge
+// @namespace    qwq0
+// @version      0.1
+// @description  Inject iiroseForge into iirose
+// @author       You
+// @match        https://iirose.com/
+// @icon         https://iirose.com/favicon.ico
+// @require      https://qwq0.github.io/iiroseForge/l.js
+// @grant        none
+// ==/UserScript==
+
+(function ()
+{
+})();
